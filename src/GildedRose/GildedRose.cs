9f7a386c1e333b94154b491c1597549637309e64
Steve Hibbott <steve.hibbott@musicmagpie.co.uk>
2024-09-13 19:36:58 +0100

    added BackstagePassStockItemStrategy to GildedRose. Removed redundant methods and properties

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -8,10 +8,6 @@ namespace GildedRose
         private const string AgedBrie = "Aged Brie";
         private const string BackstagePasses = "Backstage passes to a TAFKAL80ETC concert";
         private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
-        private const int MaxQuality = 50;
-        private const int ThresholdInDaysOfFirstQualityIncrease = 11;
-        private const int ThresholdInDaysOfSecondQualityIncrease = 6;
-
         IList<Item> Items = Items;
 
         public void UpdateQuality()
@@ -24,46 +20,12 @@ namespace GildedRose
                 }
                 else if (item.Name == BackstagePasses)
                 {
-                    UpdateBackstagePasses(item);
+                     new BackstagePassStockItemStrategy().UpdateItem(item);
                 }
                 else if (item.Name == AgedBrie)
                 {
                     new AgedBrieStockItemStrategy().UpdateItem(item);
                 }
-            }
-        }
-
-        private static void UpdateBackstagePasses(Item item)
-        {
-            IncreaseQuality(item);
-            IncreaseBackstagePassesQuality(item);
-
-            item.SellIn--;
-
-            if (item.SellIn < 0)
-            {
-                item.Quality = 0;
-            }
-        }
-
-        private static void IncreaseBackstagePassesQuality(Item item)
-        {
-            if (item.SellIn < ThresholdInDaysOfFirstQualityIncrease)
-            {
-                IncreaseQuality(item);
-            }
-
-            if (item.SellIn < ThresholdInDaysOfSecondQualityIncrease)
-            {
-                IncreaseQuality(item);
-            }
-        }
-
-        private static void IncreaseQuality(Item item)
-        {
-            if (item.Quality < MaxQuality)
-            {
-                item.Quality++;
             }
         }
     }
