e08f6c3a2fe43db01573c84c2bb71a8a51d0312e
Steve Hibbott <steve.hibbott@musicmagpie.co.uk>
2024-09-13 08:06:25 +0100

    Added more test cases and comments around testing an assumptions and fixed failing test (for now)

--- src/GildedRose/Program.cs
+++ src/GildedRose/Program.cs
@@ -11,6 +11,8 @@ namespace GildedRoseKata
 
             IList<Item> Items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                /*XXX - "Aged Brie" actually increases in Quality the older it gets - Looking at the approval test this is true but it increases by 2. 
+                Need to check if this functionality is correct but will make assumption that since this is the Golden master that the current functionality is correct */
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
                 new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
@@ -27,10 +29,32 @@ namespace GildedRoseKata
                     SellIn = 10,
                     Quality = 49
                 },
+                // XXX - This tests that Quality is not increased past quality limit
                 new Item
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 5,
+                    Quality = 49
+                },
+                // XXX - Added test coverage to show that Quality increases by 2 when there are 10 days or less
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 11,
+                    Quality = 20
+                },
+                // XXX - Added test coverage to show that Quality increases by by 3 when there are 5 days or less
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 6,
+                    Quality = 20
+                },
+                // XXX - Added test coverage to show that Quality drops to 0 after the concert
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 1,
                     Quality = 49
                 },
 				// this conjured item does not work properly yet
