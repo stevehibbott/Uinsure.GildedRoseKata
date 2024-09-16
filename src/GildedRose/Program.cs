42d26f3f357cb32a7cbde38f58d5fc492522a37d
Steve Hibbott <steve.hibbott@musicmagpie.co.uk>
2024-09-16 11:59:09 +0100

    Get Main class to use Factories and Strategy calls

--- src/GildedRose/Program.cs
+++ src/GildedRose/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using GildedRose.Factories;
 
 namespace GildedRose
 {
@@ -10,58 +11,52 @@ namespace GildedRose
             Console.WriteLine("OMGHAI!");
 
             IList<Item> Items = new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new() {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 /*XXX - "Aged Brie" actually increases in Quality the older it gets - Looking at the approval test this is true but it increases by 2. 
                 Need to check if this functionality is correct but will make assumption that since this is the Golden master that the current functionality is correct */
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                new Item
-                {
+                new() {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new() {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new() {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new() {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new() {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 15,
                     Quality = 20
                 },
-                new Item
-                {
+                new() {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 10,
                     Quality = 49
                 },
                 // XXX - This tests that Quality is not increased past quality limit
-                new Item
-                {
+                new() {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 5,
                     Quality = 49
                 },
                 // XXX - Added test coverage to show that Quality increases by 2 when there are 10 days or less
-                new Item
-                {
+                new() {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 11,
                     Quality = 20
                 },
                 // XXX - Added test coverage to show that Quality increases by by 3 when there are 5 days or less
-                new Item
-                {
+                new() {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 6,
                     Quality = 20
                 },
                 // XXX - Added test coverage to show that Quality drops to 0 after the concert
-                new Item
-                {
+                new() {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
                     SellIn = 1,
                     Quality = 49
                 },
 				// this conjured item does not work properly yet
-				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 16}
+				new() {Name = "Conjured Mana Cake", SellIn = 3, Quality = 16}
             };
 
-            var app = new GildedRose(Items);
+            var factory =  new StockItemStrategyFactory();
 
             for (var i = 0; i < 31; i++)
             {
@@ -70,9 +65,10 @@ namespace GildedRose
                 for (var j = 0; j < Items.Count; j++)
                 {
                     Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
+                    factory.Create(Items[j].Name).UpdateItem(Items[j]);
                 }
                 Console.WriteLine("");
-                app.UpdateQuality();
+
             }
         }
     }
